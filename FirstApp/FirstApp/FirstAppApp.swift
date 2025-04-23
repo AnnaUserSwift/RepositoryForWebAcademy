83553c2b891e0d4ad328f9663907204579b56c2b
Anna User <anna.lushchan@gmail.com>
2025-04-23 21:34:41 +0300

    qwerty 1

---
+++ FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Anna User on 23.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
